b084db8543004dd646c7d5dc1b6c510f82c1f98f
Ben Parr <bparr@bparr.com>
2017-03-14 11:42:32 -0400

    Make gcloud username a command line argument.

--- gcloud/main.py
+++ gcloud/main.py
@@ -1,19 +1,19 @@
+import argparse
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 import pprint
 import subprocess
 
-# TODO make this a command line option!
-GCLOUD_USERNAME = 'bparr_com'
 
 PROJECT = 'melee-ai'
 ZONE = 'us-east1-b'
 MACHINE_TYPE = 'zones/%s/machineTypes/g1-small' % ZONE
 SOURCE_IMAGE = 'projects/%s/global/images/melee-ai-2017-03-09' % PROJECT
 
+
 def getInstances(service):
   result = service.instances().list(project=PROJECT, zone=ZONE).execute()
-  pprint.pprint(result['items'])
+  #pprint.pprint(result['items'])
   return dict((x['name'], x) for x in result['items'])
 
 
@@ -39,13 +39,13 @@ def createInstance(service, name):
       project=PROJECT, zone=ZONE, body=instance_body).execute()
 
 
-def sshToInstance(instance):
+def sshToInstance(instance, username):
   host = instance['networkInterfaces'][0]['accessConfigs'][0]['natIP']
   COMMAND = 'ls'  # TODO change.
   # TODO This blocks! Make it not block.
   ssh = subprocess.Popen(
       ['ssh', '-oStrictHostKeyChecking=no', '-i',
-       '~/.ssh/google_compute_engine', GCLOUD_USERNAME + '@' + host, COMMAND],
+       '~/.ssh/google_compute_engine', username + '@' + host, COMMAND],
       shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
   result = ssh.stdout.readlines()
   if result == []:
@@ -56,10 +56,16 @@ def sshToInstance(instance):
 
 
 def main():
+  parser = argparse.ArgumentParser(description='Run Melee workers.')
+  parser.add_argument('-u', '--gcloud-username', required=True,
+                      help='gcloud ssh username.')
+  args = parser.parse_args()
+
   credentials = GoogleCredentials.get_application_default()
   service = discovery.build('compute', 'v1', credentials=credentials)
   instances = getInstances(service)
-  sshToInstance(instances['melee-ai-2017-03-14-script-test2'])
+  sshToInstance(instances['melee-ai-2017-03-14-script-test2'],
+                args.gcloud_username)
   #createInstance(service, 'melee-ai-2017-03-14-script-test2')
 
 
