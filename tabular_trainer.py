afb1d17b3266d8b7901d21abf4aee4f11e2244ef
deepakdilipkumar <deepakdilipkumar.iitb@gmail.com>
2017-04-05 22:02:23 -0400

    run off edge and terminate when match ends

--- tabular_trainer.py
+++ tabular_trainer.py
@@ -4,28 +4,8 @@ import sarsa
 
 env = SmashEnv()
 history = env.make()
-action = 0
-prev_L = True
 
-rl_model = sarsa.FullModel('qlearning', 'damage')
-# save_file = open("damage_penalty_expectedsarsa_21000.pkl", 'rb')
-# rl_model = pickle.load(save_file)
+while history[-1].state.players[1].stock != 0:
+    history = env.step(3)
 
-while True:
-    if (history[-1].frame_counter % 4 == 1):
-        action = rl_model.get_action(history)
-        cur_L = history[-1].state.players[0].controller.button_L
-
-        if (not prev_L) and cur_L:
-            rl_model.toggleExploration()
-
-        prev_L = cur_L
-    
-    history = env.step(action)
-
-    if (history[-1].frame_counter % 36000 == 0):
-        save_file = open(rl_model.reward_scheme+"_penalty_"+rl_model.model+"_"+str(rl_model.frames_trained)+".pkl", 'wb')
-        pickle.dump(rl_model, save_file)
-        save_file_2 = open(rl_model.model+"_cum_reward.pkl", 'wb')
-        pickle.dump(rl_model.cum_reward_list, save_file_2)
-        print("MODEL SAVED")
+env.terminate()
