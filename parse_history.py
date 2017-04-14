e09b26ecaded60d4f3e474381f7460ebe8a96b3d
Ben Parr <bparr@bparr.com>
2017-04-14 16:52:37 -0400

    Change tab to space.

--- parse_history.py
+++ parse_history.py
@@ -5,30 +5,30 @@ _DOING_NOTHING_STATE = 14
 
 class Parser():
 
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def parse(self, history):
+    def parse(self, history):
 
-		cur_state = history[-1].state.players[:2]
+        cur_state = history[-1].state.players[:2]
 
-		reward = 0
-		if cur_state[1].action_state == _DOING_NOTHING_STATE:
-			reward = 1
-		is_terminal = cur_state[1].percent > 0
+        reward = 0
+        if cur_state[1].action_state == _DOING_NOTHING_STATE:
+            reward = 1
+        is_terminal = cur_state[1].percent > 0
 
-		debug_info = history[-1].frame_counter
+        debug_info = history[-1].frame_counter
 
-		state = []
+        state = []
 
-		for index in range(len(cur_state)):
-			for key,_ in cur_state[index]._fields:
-				if key != 'controller':
-					val = getattr(cur_state[index], key)
-					if isinstance(val, bool):
-						val = int(val)
-					# print(key, val)
-					state.append(val)
+        for index in range(len(cur_state)):
+            for key,_ in cur_state[index]._fields:
+                if key != 'controller':
+                    val = getattr(cur_state[index], key)
+                    if isinstance(val, bool):
+                        val = int(val)
+                    # print(key, val)
+                    state.append(val)
 
 
-		return np.array(state), reward, is_terminal, debug_info
+        return np.array(state), reward, is_terminal, debug_info
