fbc0e1ddc8c2affe7841f235819bbcf6ef44f8f0
Ben Parr <bparr@bparr.com>
2017-04-15 15:05:42 -0400

    Convert tabs to spaces in smash_env.py.

--- smash_env.py
+++ smash_env.py
@@ -19,50 +19,50 @@ ACTION_TO_CONTROLLER_OUTPUT = [
 NUM_OF_ACTION = len(ACTION_TO_CONTROLLER_OUTPUT)
 
 class SmashEnv():
-	class _ActionSpace():
-		def __init__(self):
-			self.n = NUM_OF_ACTION
+    class _ActionSpace():
+        def __init__(self):
+            self.n = NUM_OF_ACTION
 
-	def __init__(self):
-		self.action_space = SmashEnv._ActionSpace()
-		self.Parser = Parser()
+    def __init__(self):
+        self.action_space = SmashEnv._ActionSpace()
+        self.Parser = Parser()
 
 
-	def make(self, args):
-    # Should only be called once
-		self.cpu, self.dolphin = run.main(args)
+    def make(self, args):
+        # Should only be called once
+        self.cpu, self.dolphin = run.main(args)
 
-		print("Running cpu.")
-		self.cpu.run(dolphin_process=self.dolphin)
-		return self.reset()
+        print("Running cpu.")
+        self.cpu.run(dolphin_process=self.dolphin)
+        return self.reset()
 
-	def step(self,action = None):
-		history = None
+    def step(self,action = None):
+        history = None
 
-		# Keep getting history until you reach a non-skipped frame
-		while history is None:
-			history = self.cpu.advance_frame(ACTION_TO_CONTROLLER_OUTPUT[action])
+        # Keep getting history until you reach a non-skipped frame
+        while history is None:
+            history = self.cpu.advance_frame(ACTION_TO_CONTROLLER_OUTPUT[action])
 
-		# Indicates that the episode ended
+        # Indicates that the episode ended
 
-		# TODO rename numbers
-		if history == 2 or history == 3 :
-			history = self.reset()
+        # TODO rename numbers
+        if history == 2 or history == 3 :
+            history = self.reset()
 
-		state, reward, is_terminal, debug_info = self.Parser.parse(history)
-		return state, reward, is_terminal, debug_info
+        state, reward, is_terminal, debug_info = self.Parser.parse(history)
+        return state, reward, is_terminal, debug_info
 
-	def reset(self):
-		history = 4
-		while history == 4:
-			history = self.cpu.advance_frame(reset_match=True)
+    def reset(self):
+        history = 4
+        while history == 4:
+            history = self.cpu.advance_frame(reset_match=True)
 
-		# After episode is ended just advance frames till match starts
-		while (history == 2 or history == 3 or history == None):
-			history = self.cpu.advance_frame(action=0)
+        # After episode is ended just advance frames till match starts
+        while (history == 2 or history == 3 or history == None):
+            history = self.cpu.advance_frame(action=0)
 
-		state, reward, is_terminal, debug_info = self.Parser.parse(history)
-		return state
+        state, reward, is_terminal, debug_info = self.Parser.parse(history)
+        return state
 
-	def terminate(self):
-		self.dolphin.terminate()
+    def terminate(self):
+        self.dolphin.terminate()
