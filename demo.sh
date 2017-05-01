5a3ab88f0c5747d510146607cb8454e81db4fd96
Ben Parr <bparr@bparr.com>
2017-04-30 22:35:47 -0400

    Add demo.sh.

---
+++ demo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python3 dqn_atari.py --gui --iso /home/bparr/dolphin/Super\ Smash\ Bros.\ Melee\ \(USA\)\ \(En\,Ja\)\ \(v1.02\).iso --cpu 9 --stage final_destination --is_worker --ai_input_dir ~/Desktop/dualing/inputs/1493471071.4083245/ --evaluate
