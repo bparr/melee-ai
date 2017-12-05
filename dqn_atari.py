97e8671c8839e6389d8713a8d3caef82f8f6412b
Ben Parr <bparr@bparr.com>
2017-12-04 23:04:08 -0500

    Update runtime.

--- dqn_atari.py
+++ dqn_atari.py
@@ -488,7 +488,7 @@ def main():  # noqa: D103
             replay_memory.append_all(worker_memories)
             append_time += time.time() - start_time
             total_appended += len(worker_memories)
-            if total_appended > 100000:
+            if total_appended > 500000:
               break
             if args.psc:
                 os.remove(memory_path)
